27f42c552e5d79839b98e10f580e9a5879117fbe
bilgejava6 <bilgejava6@gmail.com>
2025-03-27 20:58:08 +0300

    render işlemi önemli !!!!

--- reactJS/ilk-projem/src/pages/LoginPage.tsx
+++ reactJS/ilk-projem/src/pages/LoginPage.tsx
@@ -11,19 +11,42 @@
 function LoginPage() {
   /**
    * Eğer giriş butonuna basılır ise butona basıldı yazsın.
+   * ()=> {} arrow function
    */
+  let userName = '';
+  let password = '';
+  let isWrongPassword = false;
    const doLogin = ()=>{
-     alert("Butona basıldı");
+     if(userName === 'muhammet' && password === '123456'){
+          // başarılı
+          isWrongPassword = false;
+          console.log('Giriş yapıldı.');
+     }else{
+          // kullanıcı adı veya şifre hatalı
+          isWrongPassword = true;
+          console.log('Kullanıcı adı veya şifre hatalı....: ', isWrongPassword);
+     }
+     
    }  
   return (
     <>
         <h1>Login Page</h1>
         <hr />
         <div>
-             <input type="text" placeholder="Kullanıcı Adı" />
+             <input type="text" placeholder="Kullanıcı Adı" onChange={evt=>{
+               userName = evt.target.value;
+               console.log('olan olay......: ', evt);
+               }}/>
         </div>
        <div>
-            <input type="password" placeholder="Şifre" />
+            <input type="password" placeholder="Şifre" onChange={evt=> password = evt.target.value}/>
+       </div>
+       <div>
+          {
+               // hatalı bilgi girşi yapılmış ve bu değer true olmuş ise
+               isWrongPassword &&  <label style={{color: 'red'}}>* Kullanıcı adı ya da şifre hatalıdır</label>
+          }
+         
        </div>
         <div>
              <button onClick={doLogin}>Giriş Yap</button>
